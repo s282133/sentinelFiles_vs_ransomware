7e8d43ef5294187e5ffdcc2887f7e427a7fea030
s282133 <s282133@studenti.polito.it>
2022-06-02 17:24:08 +0200

    added prints

--- TEST_MIO/provaRegEx.py
+++ TEST_MIO/provaRegEx.py
@@ -1,8 +1,8 @@
 import re
 import json
 
-currBlackListFile = open("blacklist.json", "r")
-currBlackList = json.load(currBlackListFile)      # dictionary
+# currBlackListFile = open("blacklist.json", "r")
+# currBlackList = json.load(currBlackListFile)      # dictionary
 
 p = re.compile("PoliTo/C4ES/.+/attack")
 
@@ -14,13 +14,16 @@ res3 = bool(p.match("PoliTo/C4ES//attack"))
 
 res4 = bool(p.match("PoliTo/C4ES/pino/attack"))
 
+res5 = bool(p.match("PoliTo/C4ES/TEST/attack"))
+
 print(res1)
 print(res2)
 print(res3)
 print(res4)
+print(res5)
 
-currBanList = currBlackList["ban_list"]
-newBlackList = {'ban_list': [{'clientID': 'pi0', 'banTime': 1654098658.2481802, 'altered_file': '/home/pi/Desktop/fs_creato/A/TR/.samples_oH.txt', 'untrusted_topic': 'PoliTo/C4ES/pi0/#'}]}
-newBlackListFile = open("blacklist.json", "w")
-json.dump(newBlackList, newBlackListFile, indent=4)
+# currBanList = currBlackList["ban_list"]
+# newBlackList = {'ban_list': [{'clientID': 'pi0', 'banTime': 1654098658.2481802, 'altered_file': '/home/pi/Desktop/fs_creato/A/TR/.samples_oH.txt', 'untrusted_topic': 'PoliTo/C4ES/pi0/#'}]}
+# newBlackListFile = open("blacklist.json", "w")
+# json.dump(newBlackList, newBlackListFile, indent=4)
 
--- TEST_MIO/pubsubTEST.py
+++ TEST_MIO/pubsubTEST.py
@@ -1,31 +1,38 @@
 from time import sleep
 from MyMQTT import *
+import paho.mqtt.client as PahoMQTT
+import json
+import time
+import re
 
-TESTclientID = "TEST"
+class pubsubTEST():
 
-class testpub():
-    def __init__(self, clientID):
-        self.client = MyMQTT(clientID, "test.mosquitto.org", 1883, self)
-        self.clientID = clientID
+    def __init__(self):
+        self.client = MyMQTT("TEST", "test.mosquitto.org", 1883, self)
+        self.clientID = "TEST"
         print(f"{self.clientID} created")
-        self.start()
-        self.subTopic = "PoliTo/C4ES/#"
-        self.client.mySubscribe(self.subTopic)
+        self.start()  
+        
 
     def start (self):
-        self.client.start()  
+        self.client.start()                
 
     def myPublish(self, topic, message):
         print(f"{self.clientID} publishing {message} to topic: {topic}")
         self.client.myPublish(topic, message)
 
     def notify(self, topic, message):
-        print("sono nella notify")
+        pass
+        
+
+# cose da aggiungere: logica per cui se currTime - entryTime > threshold => riabilita il client
 
 if __name__ == "__main__":
+    
+    test = pubsubTEST()
 
-    test = testpub("TEST")
     sleep(2)
-    topic = "PoliTo/C4ES/pippo"
+
+    topic = "PoliTo/C4ES/TEST/attack"
     message = {"hash_mismatch_in" : "test.txt", "untrust_topic" : "PoliTo/C4ES/TEST/attack"}
     test.myPublish(topic, message)

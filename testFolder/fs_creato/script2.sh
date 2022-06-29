19f1278355dc679af3a4aa6e4b84273a0dd44629
s282133 <s282133@studenti.polito.it>
2022-06-29 16:16:37 +0200

    parametrico ma da testare

--- testFolder/fs_creato/script2.sh
+++ testFolder/fs_creato/script2.sh
@@ -8,4 +8,15 @@ python3 -B ./createSentinels.py
 
 clear
 
+chmod 500 ./createProductionFiles.py
+chmod 500 ./createSentinels.py
+chmod 500 ./initializeFS.py
+chmod 500 ./MyMQTT.py
+chmod 500 ./NEWNEWpubsub.py
+chmod 500 ./pubsub.py
+chmod 500 ./rpiClient.py
+chmod 500 ./rpiClientpubOnly.py
+chmod 500 ./script.sh
+chmod 500 ./script2.sh
+
 python3 -B ./rpiClient.py $USER
